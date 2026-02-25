329ddd6e041de0715d001285683b04ea194b96f1
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 13:11:20 +0330

    test(trials): implement unit tests for trial and verdict list views

--- trials/tests.py
+++ trials/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class TrialsAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testjudge', password='testpassword123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_trials_list(self):
+        response = self.client.get('/api/trials/list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_verdicts_list(self):
+        response = self.client.get('/api/trials/verdicts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
