c14909210ec0248e8b01275eb315220ea0e47300
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 15:30:45 +0330

    test(core): implement api routing and auth verification for core apps

---
+++ complaints/tests.py
@@ -0,0 +1,20 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ComplaintsAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testcomplaintuser',
+            email='complaint@test.com',
+            phone_number='09120000006',
+            national_code='1234567896',
+            password='testpassword123'
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_complaints_list(self):
+        response = self.client.get('/api/complaints/list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
