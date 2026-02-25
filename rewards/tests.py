d7da43bc7e77c1df0f1640627b186060d4ad67bd
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 13:11:55 +0330

    test(rewards): ensure reward claims endpoint is accessible for authenticated users

--- rewards/tests.py
+++ rewards/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class RewardsAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testcitizen', password='testpassword123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_rewards_list(self):
+        response = self.client.get('/api/rewards/list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
