145acb63262198df0f2c03c596097e184057da5c
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 13:10:53 +0330

    test(investigation): verify suspect and interrogation endpoints routing and auth

--- investigation/tests.py
+++ investigation/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class InvestigationAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testofficer', password='testpassword123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_suspects_list(self):
+        response = self.client.get('/api/suspects/list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_most_wanted(self):
+        response = self.client.get('/api/suspects/list/most_wanted/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_interrogations_list(self):
+        response = self.client.get('/api/suspects/interrogations/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
