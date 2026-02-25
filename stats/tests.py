ddc1f1f700ceb7e8632c0cf203d6d394174ae7b5
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 13:12:55 +0330

    test(stats): validate aggregated dashboard statistics api response

--- stats/tests.py
+++ stats/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class StatsAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testadmin', password='testpassword123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_dashboard_stats(self):
+        response = self.client.get('/api/stats/dashboard/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
