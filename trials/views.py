8549d2c497f74607a966154b07faec3708277347
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 11:41:50 +0330

    feat(trials): implement trial viewsets and routing endpoints

--- trials/views.py
+++ trials/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Trial, Verdict
+from .serializers import TrialSerializer, VerdictSerializer
 
-# Create your views here.
+class TrialViewSet(viewsets.ModelViewSet):
+    queryset = Trial.objects.all()
+    serializer_class = TrialSerializer
+
+class VerdictViewSet(viewsets.ModelViewSet):
+    queryset = Verdict.objects.all()
+    serializer_class = VerdictSerializer
