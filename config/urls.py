7011e70022a87c9d66ae3a01cbda13afb1477e37
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 14:39:27 +0330

    chore(config): integrate cases, complaints, and evidences into root urls

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
-# JWT Imports
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-# Swagger Imports
 from rest_framework import permissions  
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger configuration
 schema_view = get_schema_view(
    openapi.Info(
       title="L.A. Noire Police Department API",
@@ -23,18 +18,17 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Swagger Documentation URLs
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
-    # Auth endpoints
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/', include('accounts.urls')), 
-    
-    # Role endpoints
     path('api/', include('roles.urls')),
+
+    path('api/cases/', include('cases.urls')),
+    path('api/complaints/', include('complaints.urls')),
+    path('api/evidences/', include('evidences.urls')),
 
     path('api/suspects/', include('investigation.urls')),
     path('api/trials/', include('trials.urls')),
