3c39f6c3b296e6da945043a6951da136ae91f817
atefehghandehari <atfghndhri19@gmail.com>
2026-02-25 11:42:05 +0330

    feat(rewards): add viewsets for managing reward claims

--- rewards/views.py
+++ rewards/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import RewardClaim
+from .serializers import RewardClaimSerializer
 
-# Create your views here.
+class RewardClaimViewSet(viewsets.ModelViewSet):
+    queryset = RewardClaim.objects.all()
+    serializer_class = RewardClaimSerializer
